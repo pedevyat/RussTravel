036de72e795e3c4b9cdae262e1a6210199e684e1
Kostya <114941602+pedevyat@users.noreply.github.com>
2023-10-23 17:56:32 +0300

    add maps

--- russ_travel/ios/Runner/AppDelegate.swift
+++ russ_travel/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import YandexMapsMobile
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    YMKMapKit.setLocale("YOUR_LOCALE") // Your preferred language. Not required, defaults to system language
+    YMKMapKit.setApiKey("228ecb8e-cc47-45da-a808-d6b319bb3f32") // Your generated API key
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
