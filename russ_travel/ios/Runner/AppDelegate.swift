3952d77617fd67ba6f072c9817ced90a663766a8
Kostya <114941602+pedevyat@users.noreply.github.com>
2023-11-09 16:55:31 +0300

    Добавление карты на ios

--- russ_travel/ios/Runner/AppDelegate.swift
+++ russ_travel/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import YandexMapsMobile
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    YMKMapKit.setApiKey("228ecb8e-cc47-45da-a808-d6b319bb3f32")
+    YMKMapKit.setApiKey("2aae07a3-9487-4765-835c-3da79cf052b7")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
