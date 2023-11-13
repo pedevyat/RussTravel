d1bcb148fa79bef7c71c3429550d69dab669338b
Kostya <114941602+pedevyat@users.noreply.github.com>
2023-11-13 18:15:56 +0300

    Добавление ButtonNavigationBar

--- russ_travel/lib/main.dart
+++ russ_travel/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 import 'map/presentation/screens/map_screen.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,11 +12,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Yandex Map',
+      //title: 'Yandex Map',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: const MapScreen(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    const MapScreen(),
+    Container(),
+    Container(),
+    // Add other pages/screens as needed
+    // Example: const YourOtherScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map_outlined),
+            label: 'Карта',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list_alt),
+            label: 'Статьи',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info_outline),
+            label: 'О приложении',
+          ),
+        ],
+      ),
     );
   }
 }
