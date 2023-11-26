9f0b3b97c71b9ba2537fa692bef8e9fa7e7cb9d7
MihailPohodeev <mishapohodeev@yandex.ru>
2023-11-26 18:36:10 +0300

    Revert "Added "Sign In" and "Sign Up" pages"
    This reverts commit 0413d9cb85b72fe3fb433dc9e42471f87bce69da.

--- russ_travel/lib/main.dart
+++ russ_travel/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 import 'map/presentation/screens/map_screen.dart';
-import 'profile.dart';
 
 
 void main() {
@@ -18,58 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-<<<<<<< Updated upstream
       home: const MapScreen(),
-=======
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-  final List<Widget> _pages = [
-    MapScreen(),
-    Container(),
-    Profile(),
-    // Add other pages/screens as needed
-    // Example: const YourOtherScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.map_outlined),
-            label: 'Карта',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list_alt),
-            label: 'Статьи',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.perm_identity),
-            label: 'Профиль',
-          ),
-        ],
-      ),
->>>>>>> Stashed changes
     );
   }
 }
