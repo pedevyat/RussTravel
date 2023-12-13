47ae8906d92e570beb00f87695493b8b888da215
Kostya <114941602+pedevyat@users.noreply.github.com>
2023-12-13 22:31:00 +0300

    добавил скролл в инфо и добавил функцию чтения файла в осетии

--- russ_travel/lib/articles/regions/osetia.dart
+++ russ_travel/lib/articles/regions/osetia.dart
@@ -1,31 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Osetia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Image.network(
-              'https://vmeste-rf.tv/upload/resize_cache/iblock/7b1/1040_650_2/lori_0000039468_a6.jpg',
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Северная Осетия-Алания – почти самая компактная в плане территории из республик Северного Кавказа. Меньше только соседняя Ингушетия. Поэтому на знакомство с самыми главными достопримечательностями хватит 3-4 дня. ' +
-                'Но на этой территории такое множество природных красот и исторических памятников, что для полноценного изучения здешних мест в самостоятельном режиме мы бы рекомендовали неделю. ' +
-                'Республика славится интересными локациями, живописной природой и гостеприимными жителями. Разработано множество туристических направлений для последовательного ознакомления гостей Северной Осетии с ее достопримечательностями. Практически во все туры включено посещение горных аулов Цамад, Лац и Цымити, старинных святилищ, Даргавского некрополя, Дзивгисской крепости и других удивительных мест. ' +
-                  'Материал: https://bolshayastrana.com/blog/severnaya-osetiya-letom-80'  ,
-                style: TextStyle(fontSize: 17),
-              ),
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: FutureBuilder(
+          future: loadDescription(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.network(
+                    'https://historyrussia.org/images/Region/Vladikavkaz/s1200.jpg',
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      snapshot.data ?? '',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
-        ),
+      ),
     );
+  }
+
+
+  Future<String> loadDescription() async {
+    return await rootBundle.loadString('assets/north_caucasus/osetia.txt');
   }
 }
--- russ_travel/lib/information/info_screen.dart
+++ russ_travel/lib/information/info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class InformationWidget extends StatelessWidget {
   const InformationWidget({super.key});
@@ -10,24 +11,41 @@ class InformationWidget extends StatelessWidget {
       appBar: AppBar(
         title: Text('О приложении'),
       ),
-      body:
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-          Image.asset('assets/splash.png',
-          height: 200,
-          fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: FutureBuilder(
+          future: loadDescription(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.asset(
+                    'assets/splash.png',
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      snapshot.data ?? '',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
         ),
-        Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Text(
-              ' Мобильное приложение, которое поможет путешественникам планировать свои поездки. Оно включает в себя функции по поиску отелей, местных достопримечательностей.' +
-              'Также будут предусмотрены интегрированные карты и советы по путешествиям.',
-            style: TextStyle(fontSize: 18),
-          ),
-        ),
-      ],
-    )
+      ),
     );
+  }
+
+
+  Future<String> loadDescription() async {
+    return await rootBundle.loadString('assets/info.txt');
   }
 }
