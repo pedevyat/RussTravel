5778de5d9f6bdb08ce3133fd1ed571fae1e30a53
Kostya <114941602+pedevyat@users.noreply.github.com>
2024-02-17 20:51:52 +0300

    добавил кнопку поиска и фильтраций заголовок в разделе статьей

--- russ_travel/lib/articles/screens/article_list_screen.dart
+++ russ_travel/lib/articles/screens/article_list_screen.dart
@@ -15,10 +15,17 @@ import '../regions/north_caucaus/osetia.dart';
 import '../regions/north_caucaus/stavropol.dart';
 import 'article_detail_screen.dart';
 
+// ДОБАВИТЬ ИЗОБРАЖЕНИЕ, ЕСЛИ НЕТ РЕЗУЛЬТАТОВ ПОИСКА
 
+class ArticleListScreen extends StatefulWidget {
+  @override
+  _ArticleListScreenState createState() => _ArticleListScreenState();
+}
 
-class ArticleListScreen extends StatelessWidget {
-  final List<Article> articles = [
+class _ArticleListScreenState extends State<ArticleListScreen> {
+  bool _ascendingOrder = true;
+  TextEditingController _searchController = TextEditingController();
+  List<Article> articles = [
     Article(title: "Республика Ингушетия", content: Ingushetia()),
     Article(title: "Чеченская республика", content: Chechnya()),
     Article(title: "Республика Северная Осетия - Алания", content: Osetia()),
@@ -26,37 +33,64 @@ class ArticleListScreen extends StatelessWidget {
     Article(title: "Республика Дагестан", content: Dagestan()),
     Article(title: "Ставропольский край: Кавказские Минеральные Воды", content: Kmv()),
     Article(title: "Ставропольский край", content: Stavropol()),
-
     Article(title: "Белгородская область", content: Belgorod()),
     Article(title: "Брянская область", content: Bryansk()),
     Article(title: "Владимирская область", content: VladimirRegion()),
     Article(title: "Владимирская область: Владимир", content: VladimirCity()),
     Article(title: "Воронежская область", content: Voronezh()),
     Article(title: "Ивановская область", content: Ivanovo()),
-    Article(title: "Калужская область", content: Kaluga())
+    Article(title: "Калужская область", content: Kaluga()),
   ];
+
+  List<Article> _filteredArticles = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredArticles.addAll(articles);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Статьи'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.sort),
+            onPressed: _sortArticles,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: _searchArticles,
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
+            TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Поиск',
+                hintText: 'Поиск места...',
+              ),
+              onChanged: (value) {
+                _filterArticles(value);
+              },
+            ),
             ListView.builder(
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
-              itemCount: articles.length,
+              itemCount: _filteredArticles.length,
               itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text(articles[index].title),
+                  title: Text(_filteredArticles[index].title),
                   onTap: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => ArticleDetailScreen(article: articles[index]),
+                        builder: (context) => ArticleDetailScreen(article: _filteredArticles[index]),
                       ),
                     );
                   },
@@ -69,4 +103,51 @@ class ArticleListScreen extends StatelessWidget {
     );
   }
 
+  void _sortArticles() {
+    setState(() {
+      if (_ascendingOrder) {
+        articles.sort((a, b) => a.title.compareTo(b.title));
+      } else {
+        articles.sort((a, b) => b.title.compareTo(a.title));
+      }
+      _ascendingOrder = !_ascendingOrder;
+      _filteredArticles.clear();
+      _filteredArticles.addAll(articles);
+    });
+  }
+
+  void _searchArticles() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Поиск места'),
+          content: TextField(
+            controller: _searchController,
+            decoration: InputDecoration(
+              labelText: 'Поиск',
+              hintText: 'Поиск места...',
+            ),
+            onChanged: (value) {
+              _filterArticles(value);
+            },
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Отмена'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _filterArticles(String query) {
+    setState(() {
+      _filteredArticles = articles.where((article) => article.title.toLowerCase().contains(query.toLowerCase())).toList();
+    });
+  }
 }
