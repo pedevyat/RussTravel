428df60aaa50975cf85289fa7f1de0ccb2db26e6
Zxctolas <nano.buster0@yandex.ru>
2024-04-23 18:33:05 +0300

    добавил описание областей

---
+++ russ_travel/lib/articles/regions/privolzhye/Ylyanovskaya.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Ylyanovskaya extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: FutureBuilder(
+          future: loadDescription(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.network(
+                    'https://arxiteka.ru/gallery_gen/a652120b1a16c0d9e094e928f820c7f5.jpg',
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      snapshot.data ?? '',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<String> loadDescription() async {
+    return await rootBundle.loadString('assets/privolzhye/Ylyanovskaya.txt');
+  }
+}
---
+++ russ_travel/lib/articles/regions/ural/Sverdlovskaya.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Sverdlovskaya extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: FutureBuilder(
+          future: loadDescription(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Image.network(
+                    'https://vskali.ru/wp-content/uploads/2022/12/image2.png',
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      snapshot.data ?? '',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                ],
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+
+  Future<String> loadDescription() async {
+    return await rootBundle.loadString('assets/ural/Sverdlovskaya.txt');
+  }
+}
