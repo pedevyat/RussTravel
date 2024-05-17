a362aa41217731a4a8ef5b814de484970ab81ae8
Kostya <114941602+pedevyat@users.noreply.github.com>
2024-05-17 16:27:20 +0300

    добавлены путеводители

--- russ_travel/lib/articles/screens/article_list_screen.dart
+++ russ_travel/lib/articles/screens/article_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:russ_travel/articles/regions/DV/Komsomolsk-na-Amure.dart';
+import 'package:russ_travel/articles/regions/DV/Ulan-Ude.dart';
 import 'package:russ_travel/articles/regions/center/belgorod.dart';
 import 'package:russ_travel/articles/regions/center/ivanovo.dart';
 import 'package:russ_travel/articles/regions/center/kostroma.dart';
@@ -22,6 +24,49 @@ import 'package:russ_travel/articles/regions/ural/Kurganskaya.dart';
 import 'package:russ_travel/articles/regions/ural/Sverdlovskaya.dart';
 import 'package:russ_travel/articles/regions/ural/Tumenskaya.dart';
 import 'package:russ_travel/articles/regions/ural/Yamalo-neneckii.dart';
+import '../regions/DV/Amursk.dart';
+import '../regions/DV/Anadyr.dart';
+import '../regions/DV/Artem.dart';
+import '../regions/DV/Belogorsk.dart';
+import '../regions/DV/Birobidzhan.dart';
+import '../regions/DV/Blagoveshensk.dart';
+import '../regions/DV/Chita.dart';
+import '../regions/DV/Elizovo.dart';
+import '../regions/DV/Gusinoozersk.dart';
+import '../regions/DV/Habarovsk.dart';
+import '../regions/DV/Holmsk.dart';
+import '../regions/DV/Korsakov.dart';
+import '../regions/DV/Krasnokamensk.dart';
+import '../regions/DV/Kyachta.dart';
+import '../regions/DV/Magadan.dart';
+import '../regions/DV/Milkovo.dart';
+import '../regions/DV/Mirniy.dart';
+import '../regions/DV/Nahodka.dart';
+import '../regions/DV/Nerchinsk.dart';
+import '../regions/DV/Neryungri.dart';
+import '../regions/DV/Petropavlovsk-kamchatskiy.dart';
+import '../regions/DV/Severobaikalsk.dart';
+import '../regions/DV/Tynda.dart';
+import '../regions/DV/Ussuriysk.dart';
+import '../regions/DV/Vladivostok.dart';
+import '../regions/DV/Yakutsk.dart';
+import '../regions/DV/Yuzhno-Sachalinsk.dart';
+import '../regions/Yufo/Astrachan.dart';
+import '../regions/Yufo/Astrachan.dart';
+import '../regions/Yufo/Elista.dart';
+import '../regions/Yufo/Kerch.dart';
+import '../regions/Yufo/Krasnodar.dart';
+import '../regions/Yufo/Maikop.dart';
+import '../regions/Yufo/Novocherkassk.dart';
+import '../regions/Yufo/Novorossiysk.dart';
+import '../regions/Yufo/Rostov.dart';
+import '../regions/Yufo/Semfiropol.dart';
+import '../regions/Yufo/Sevastopol.dart';
+import '../regions/Yufo/Shachty.dart';
+import '../regions/Yufo/Sochi.dart';
+import '../regions/Yufo/Volgograd.dart';
+import '../regions/Yufo/Volzhskiy.dart';
+import '../regions/Yufo/Yalta.dart';
 import '../regions/center/bryansk.dart';
 import '../regions/center/kaluga.dart';
 import '../regions/center/oryol.dart';
@@ -35,6 +80,20 @@ import '../regions/north_caucaus/kchr.dart';
 import '../regions/north_caucaus/kmv.dart';
 import '../regions/north_caucaus/osetia.dart';
 import '../regions/north_caucaus/stavropol.dart';
+import '../regions/privolzhye/Bashkotorstan.dart';
+import '../regions/privolzhye/Chyvashiya.dart';
+import '../regions/privolzhye/Kirovskaya.dart';
+import '../regions/privolzhye/Marii_el.dart';
+import '../regions/privolzhye/Mordoviya.dart';
+import '../regions/privolzhye/Nizhegorodskaya.dart';
+import '../regions/privolzhye/Orenburgskaya.dart';
+import '../regions/privolzhye/Penzenskaya.dart';
+import '../regions/privolzhye/Permskii.dart';
+import '../regions/privolzhye/Samarskaya.dart';
+import '../regions/privolzhye/Saratovskaya.dart';
+import '../regions/privolzhye/Tatarstan.dart';
+import '../regions/privolzhye/Ydmurtiya.dart';
+import '../regions/privolzhye/Ylyanovskaya.dart';
 import '../regions/ural/Yamalo-neneckii.dart';
 import 'article_detail_screen.dart';
 
@@ -89,7 +148,7 @@ class _ArticleListScreenState extends State<ArticleListScreen> {
     Article(title: "Ханты-Мансийский АО", content: Hanti_Mansiiskii()),
     Article(title: "Челябинская область", content: Chelyabinskaya()),
     
-    Article(title: "Республика Башкортостан", content: bashkortorstan()),
+    Article(title: "Республика Башкортостан", content: Bashkotorstan()),
     Article(title: "Республика Чувашия", content: Chyvashiya()),
     Article(title: "Кировская область", content: Kirovskaya()),
     Article(title: "Республика Марий Эл", content: Marii_el()),
@@ -112,11 +171,11 @@ class _ArticleListScreenState extends State<ArticleListScreen> {
     Article(title: "Новочеркасск", content: Novocherkassk()),
     Article(title: "Новороссийск", content: Novorossiysk()),
     Article(title: "Ростовская область", content: Rostov()),
-    Article(title: "Семфирополь", content: Semfiropol()),
+    Article(title: "Симфирополь", content: Semfiropol()),
     Article(title: "Севастополь", content: Sevastopol()),
     Article(title: "Шахты", content: Shachty()),
     Article(title: "Сочи", content: Sochi()),
-    Article(title: "Волгоградская область", content: Rostov()),
+    Article(title: "Волгоградская область", content: Volgograd()),
     Article(title: "Волжский", content: Volzhskiy()),
     Article(title: "Ялта", content: Yalta()),
 
@@ -131,24 +190,24 @@ class _ArticleListScreenState extends State<ArticleListScreen> {
     Article(title: "Гусиноозёрск", content: Gusinoozersk()),
     Article(title: "Хабаровский край", content: Habarovsk()),
     Article(title: "Холмск", content: Holmsk()),
-    Article(title: "Комсомольск-на-Амуре", content: Komsomolsk-na-Amure()),
+    Article(title: "Комсомольск-на-Амуре", content: Komsomolsk_na_Amure()),
     Article(title: "Корсаков", content: Korsakov()),
     Article(title: "Краснокаменск", content: Krasnokamensk()),
-    Article(title: "Кяхта", content: Kyahta()),
+    Article(title: "Кяхта", content: Kyachta()),
     Article(title: "Магаданская область", content: Magadan()),
     Article(title: "Милково", content: Milkovo()),
     Article(title: "Мирный", content: Mirniy()),
     Article(title: "Находка", content: Nahodka()),
     Article(title: "Нерчинск", content: Nerchinsk()),
     Article(title: "Нерюнгри", content: Neryungri()),
-    Article(title: "Камчатский край", content: Petropavlovsk-Kamchatskiy()),
+    Article(title: "Камчатский край", content: Petropavlovsk_kamchatskiy()),
     Article(title: "Северобайкальск", content: Severobaikalsk()),
     Article(title: "Тунда", content: Tynda()),
-    Article(title: "Республика Бурятия", content: Ulan-Ude()),
+    Article(title: "Республика Бурятия", content: Ulan_Ude()),
     Article(title: "Уссурийск", content: Ussuriysk()),
     Article(title: "Приморский край", content: Vladivostok()),
     Article(title: "Республика Саха (Якутия)", content: Yakutsk()),
-    Article(title: "Сахалинская область", content: Yuzhno-Sachalinsk()),
+    Article(title: "Сахалинская область", content: Yuzhno_Sachalinsk()),
 
   ];
 
