cd22dff3d298f81eaba9d51c0df26bec9593af19
MihailPohodeev <115114201+MihailPohodeev@users.noreply.github.com>
2024-05-17 23:34:45 +0300

    Revert "change dependencies"
    This reverts commit c7e0a69aa27038eafb8e100b7705f0ba62c38130.

--- russ_travel/lib/articles/regions/center/kaluga.dart
+++ russ_travel/lib/articles/regions/center/kaluga.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class Kaluga extends StatelessWidget {
   @override
@@ -22,7 +20,10 @@ class Kaluga extends StatelessWidget {
                   ),
                   Padding(
                     padding: EdgeInsets.all(16.0),
-                    child: buildDescriptionWidget(snapshot.data ?? ''),
+                    child: Text(
+                      snapshot.data ?? '',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
                 ],
               );
@@ -37,37 +38,6 @@ class Kaluga extends StatelessWidget {
     );
   }
 
-  Widget buildDescriptionWidget(String description) {
-    final RegExp urlRegExp = RegExp(r'http(s)?://\S+');
-    final Iterable<Match> matches = urlRegExp.allMatches(description);
-
-    List<TextSpan> textSpans = [];
-
-    int start = 0;
-    for (Match match in matches) {
-      if (match.start > start) {
-        textSpans.add(TextSpan(text: description.substring(start, match.start)));
-      }
-
-      String url = description.substring(match.start, match.end);
-      textSpans.add(TextSpan(
-        text: url,
-        style: TextStyle(color: Colors.blue),
-        recognizer: TapGestureRecognizer()
-          ..onTap = () {
-            launch(url);
-          },
-      ));
-
-      start = match.end;
-    }
-
-    if (start < description.length) {
-      textSpans.add(TextSpan(text: description.substring(start)));
-    }
-
-    return Text.rich(TextSpan(children: textSpans));
-  }
 
   Future<String> loadDescription() async {
     return await rootBundle.loadString('assets/center/kaluga.txt');
