56e90ff03566906d34b0ceec396b3475576f8d3c
MihailPohodeev <115114201+MihailPohodeev@users.noreply.github.com>
2024-05-19 18:21:22 +0300

    change pubsec.yaml

--- russ_travel/macos/Flutter/GeneratedPluginRegistrant.swift
+++ russ_travel/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_macos
-import geolocator_apple
 import path_provider_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
